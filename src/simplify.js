b6ea08ecd4fa4c76b5d46abcf94cd8e449451867
Peter Olson <peter.e.c.olson+github@gmail.com>
2019-03-27 00:41:52 -0500

    traditionalize 只 and 里

--- src/simplify.js
+++ src/simplify.js
@@ -1,5 +1,6 @@
 let segment = require("./segment"),
     { s2tDict, t2sDict } = require("./pinyinDict");
+let nodejieba = require("nodejieba");
 
 function simplify(text) {
     return segment(text).map(x => {
@@ -8,11 +9,28 @@ function simplify(text) {
     }).join("");
 }
 
+const specialChars = new Set(["只"]);
+
 function traditionalize(text) {
-    return segment(text).map(x => {
+    return segment(text).map((x, i, segments) => {
+        if (specialChars.has(x)) {
+            return traditionalizeSpecialChar(x, segments.slice(0, i), segments.slice(i + 1));
+        }
         if (x in s2tDict) return s2tDict[x];
         return x;
     }).join("");
+}
+
+function traditionalizeSpecialChar(char, beforeText, afterText) {
+    switch (char) {
+        case "只":
+            let prev = nodejieba.tag(beforeText.join("")).slice(-1)[0];
+            let after = nodejieba.tag(afterText.join(""))[0];
+            if (prev && prev.tag === "m") return "隻";
+            if (after && after.tag === "n") return "隻";
+            return "只";
+    }
+    return char in s2tDict ? s2tDict[char] : char;
 }
 
 module.exports = {
